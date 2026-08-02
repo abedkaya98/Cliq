aa0168c9687f20f4e519d9205135ea0c6d549b3e
abedkaya98 <70577427+abedkaya98@users.noreply.github.com>
2026-08-02 16:15:32 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/cliqnotifier/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.cliqnotifier
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cliqnotifier.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnTest.setOnClickListener {
+            Toast.makeText(this, "النسخة النقية تعمل 100% بدون أي كراش!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
