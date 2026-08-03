bec9bf6a1e4b27b2ec0c5d82303d63a4d799b31c
abedkaya98 <70577427+abedkaya98@users.noreply.github.com>
2026-08-03 18:51:09 +0300

    update app/src/main/java/com/example/cliqnotifier/FilterRule.kt

--- app/src/main/java/com/example/cliqnotifier/FilterRule.kt
+++ app/src/main/java/com/example/cliqnotifier/FilterRule.kt
@@ -2,9 +2,9 @@ package com.example.cliqnotifier
 
 data class FilterRule(
     val id: String = System.currentTimeMillis().toString(),
-    var bankSenderId: String,      // اسم مرسل الـ SMS (مثلاً: Reflect أو CAB)
-    var requiredKeyword: String,   // كلمة ملزمة بالرسالة (مثلاً: CliQ)
-    var amountPrefix: String,      // الكلمة قبل المبلغ (مثلاً: مبلغ)
-    var amountSuffix: String,      // الكلمة بعد المبلغ (مثلاً: JOD)
-    var senderPrefix: String       // الكلمة قبل اسم الشخص (مثلاً: من)
+    var bankSenderId: String = "",      // اسم مرسل الـ SMS (مثلاً: Reflect أو CAB)
+    var requiredKeyword: String = "",   // كلمة ملزمة بالرسالة (مثلاً: CliQ)
+    var amountPrefix: String = "",      // الكلمة قبل المبلغ (مثلاً: مبلغ)
+    var amountSuffix: String = "",      // الكلمة بعد المبلغ (مثلاً: JOD)
+    var senderPrefix: String = ""       // الكلمة قبل اسم الشخص (مثلاً: من)
 )
